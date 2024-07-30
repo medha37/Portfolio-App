9937441718d7e47f05e08ecf61798b202f67d8f7
Medha Shreyashi <medhashreyashi@Medhas-MacBook-Air.local>
2024-07-30 22:44:48 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
-import './App.css'
+import styles from './App.module.css'
+import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    </>
+    <div className={styles.App}>
+    <Navbar/>
+    </div>
   )
 }
 
